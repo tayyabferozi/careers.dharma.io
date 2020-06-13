1a4978e4b01ac62c0821db7d17c04a901fa23839
Tayyab Ferozi <ferozitayyab@gmail.com>
2020-06-13 05:48:55 -0700

    fix the responsive issues

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -5,11 +5,12 @@ import "./Article.css"
 const Article = props => {
   const direction = (props.alignment === "left" ? "row" : "row-reverse");
   const ColTextPaddingStyle = (direction === "row" ? { paddingLeft: "5.5rem" } : { paddingRight: "5.5rem" });
+  const ColImgMarginStyle = (direction === "row" ? { marginRight: "40px" } : { marginLeft: "40px" });
   return (
     <div className="Article">
       <div className="Row" style={{ flexDirection: direction }}>
-        <div className="Col">
-          <img src={props.img} alt={props.imageAlt} className="ArticleImage" />
+        <div className="Col ColImg">
+          <img src={props.img} alt={props.imageAlt} className="ArticleImage" style={ColImgMarginStyle} />
         </div>
         <div className="Col ColText" style={ColTextPaddingStyle}>
           <h2>{props.SectionTitle}</h2>
